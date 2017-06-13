2c58710110a2fbb8fe4dd25f7329d8c00c1eff13
xafero <georg.berendt@uni-oldenburg.de>
2017-06-13 22:53:55 +0200

    Added a virtual Mac support

---
+++ HealthTracker.Mac/MacHealthHub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using HealthTracker.API;
+
+namespace HealthTracker.Mac
+{
+    public class MacHealthHub : IHealthHub
+    {
+        private Thread thread;
+
+        public MacHealthHub()
+        {
+            thread = new Thread(Rand);
+            thread.Start();
+        }
+
+        private void Rand()
+        {
+            var rnd = new Random();
+            while (true)
+            {
+                OnHealthEvent?.Invoke(this, new SimpleData
+                {
+                    Data = (int)(rnd.NextDouble() * 100),
+                    Kind = DeviceKind.Scale,
+                    Origin = 42,
+                    Status = DataKind.Transitional,
+                    Time = DateTime.Now,
+                    Unit = Unit.kg
+                });
+                Thread.Sleep(5 * 100);
+            }
+        }
+
+        public event DataHandler OnHealthEvent;
+
+        public void Dispose()
+        {
+            thread.Abort();
+        }
+    }
+}
--- HealthTracker/MainForm.cs
+++ HealthTracker/MainForm.cs
@@ -12,7 +12,10 @@ namespace HealthTracker
         {
             InitializeComponent();
             Type hubType;
-            if (Environment.OSVersion.Platform == PlatformID.Unix)
+            var isApple = Environment.GetEnvironmentVariable("Apple_PubSub_Socket_Render") != null;
+            if (isApple)
+                hubType = Type.GetType("HealthTracker.Mac.MacHealthHub, HealthTracker.Mac");
+            else if (Environment.OSVersion.Platform == PlatformID.Unix)
                 hubType = Type.GetType("HealthTracker.Linux.LinuxHealthHub, HealthTracker.Linux");
             else
                 hubType = Type.GetType("HealthTracker.WinRT.WindowsHealthHub, HealthTracker.WinRT");
@@ -23,6 +26,8 @@ namespace HealthTracker
 
         private void Hub_OnHealthEvent(IHealthHub hub, IDataEvent data)
         {
+            if (!currentMeasure.IsHandleCreated)
+                return;
             currentMeasure.BeginInvoke((Action)(() =>
             {
                 currentMeasure.Text = $"{data.Data} {data.Unit} ({(data.Status == DataKind.Final ? "!" : "~")})";
