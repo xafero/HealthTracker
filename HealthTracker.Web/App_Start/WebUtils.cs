72615b8b948fc929248713ff45aa1569ee9214b2
xafero <georg.berendt@uni-oldenburg.de>
2017-06-24 16:09:20 +0200

    Added DB

---
+++ HealthTracker.Web/App_Start/WebUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace HealthTracker.Web
+{
+    public static class WebUtils
+    {
+        public static void SetCurrentDir(string root)
+        {
+            if (!Directory.Exists(root))
+                Directory.CreateDirectory(root);
+            Environment.CurrentDirectory = root;
+        }
+    }
+}
--- HealthTracker.Web/Global.asax.cs
+++ HealthTracker.Web/Global.asax.cs
@@ -1,6 +1,10 @@
-﻿using System.Web;
+﻿using HealthTracker.Data;
+using System;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using static HealthTracker.Web.WebUtils;
 
 namespace HealthTracker.Web
 {
@@ -8,6 +12,8 @@ namespace HealthTracker.Web
     {
         protected void Application_Start()
         {
+            SetCurrentDir(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data"));
+            Database.Init();
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
