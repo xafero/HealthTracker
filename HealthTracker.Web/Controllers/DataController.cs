adc855afb3c15e5f375d1359259d793be6070b15
xafero <georg.berendt@uni-oldenburg.de>
2017-06-24 20:05:44 +0200

    Killed winforms

---
+++ HealthTracker.Web/Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.Mvc;
+using HealthTracker.API;
+
+namespace HealthTracker.Web.Controllers
+{
+    public class DataController : Controller
+    {
+        public static void OnHealthEvent(IHealthHub hub, IDataEvent data)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*
+         *  private void Hub_OnHealthEvent(IHealthHub hub, IDataEvent data)
+        {
+            if (!currentMeasure.IsHandleCreated)
+                return;
+            currentMeasure.BeginInvoke((Action)(() =>
+            {
+                currentMeasure.Text = $"{data.Data} {data.Unit} ({(data.Status == DataKind.Final ? "!" : "~")})";
+            }));
+        }   */
+    }
+}
--- HealthTracker.Web/Global.asax.cs
+++ HealthTracker.Web/Global.asax.cs
@@ -1,7 +1,10 @@
 ﻿using HealthTracker.API;
+using HealthTracker.Core;
 using HealthTracker.Data;
+using HealthTracker.Web.Controllers;
 using System;
 using System.IO;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -11,14 +14,44 @@ namespace HealthTracker.Web
 {
     public class Global : HttpApplication
     {
-        private static readonly IHealthHub hub = HealthHubFactory.CreateHub();
+        private static PersonRepository persons;
+        private static IHealthHub hub;
+        private static CalculateHub calc;
 
         protected void Application_Start()
         {
             SetCurrentDir(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data"));
             Database.Init();
+            persons = new PersonRepository();
+            var person = persons.List().LastOrDefault();
+            if (person == null)
+            {
+                persons.Save(new Person
+                {
+                    Birthday = DateTime.Now,
+                    FirstName = "Test",
+                    LastName = "User",
+                    Height = 175,
+                    Sex = Sex.Male
+                });
+                person = persons.List().Last();
+            }
+            hub = HealthHubFactory.CreateHub();
+            hub.OnHealthEvent += DataController.OnHealthEvent;
+            calc = new CalculateHub(person);
+            hub.OnHealthEvent += calc.OnRawHealthEvent;
+            calc.OnHealthEvent += DataController.OnHealthEvent;
+            // Stop listener
+            AppDomain.CurrentDomain.DomainUnload += CurrentDomain_DomainUnload;
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_DomainUnload;
+            // ASP.NET
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        private void CurrentDomain_DomainUnload(object sender, EventArgs e)
+        {
+            hub.Dispose();
         }
     }
 }
