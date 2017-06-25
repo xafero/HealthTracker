b8a55a7a1d3f10d71dc5e010673a5c3d6d5c00d1
xafero <georg.berendt@uni-oldenburg.de>
2017-06-25 17:26:23 +0200

    Integrated Windows module does not work with web app

--- HealthTracker.WinRT/Extensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.Remoting.Metadata.W3cXsd2001;
-
-namespace HealthTracker.WinRT
-{
-    public static class Extensions
-    {
-        public static string ToHex(this byte[] bytes) => (new SoapHexBinary(bytes)).ToString();
-    }
-}
